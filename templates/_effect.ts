72b53a41bddc8f3945b347bae1e6f062f60268ec
Duncan Hunter <duncan@duncanhunter.com.au>
2017-10-04 12:08:04 +1000

    update casing to proper case and dashcase

--- templates/_effect.ts
+++ templates/_effect.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
-import * as {{ camelCase name }}Actions from '{{position "actions"}}/{{ lowerCase name }}.actions';
+import * as {{camelCase name}}Actions from '{{position "actions"}}/{{dashCase name}}.actions';
 import { switchMap, catch, map } from 'rxjs/operators';
 import { of } from 'rxjs/Observable/of';
-import { {{ titleCase name}}Service } from './../swagger-client';
+import { {{properCase name}}Service } from './../swagger-client';
 import { ErrorHandlerService } from '../../services/error-handler.service';
 
 @Injectable()
@@ -14,12 +14,12 @@ export class {{ properCase name }}Effects {
      .pipe(
         ofType({{ camelCase name }}Actions.LOAD_{{upperCase name}}),
         switchMap(payload => this.{{camelCase name}}Service.get(),
-        map(data => ({ type: {{camelCase name}}Actions.LOAD_{{upperCase name }}_SUCCESS, payload: data})),
+        map(data => ({ type: {{camelCase name}}Actions.LOAD_{{upperCase name }}_SUCCESS, payload: data}),
         catch(() => of({ type: {{camelCase name}}Actions.LOAD_{{upperCase name }}_FAIL}))
      ));
 
   constructor(
-    private {{ camelCase name }}Service: {{ properCase name }}Service,
+    private {{ camelCase name }}Service: {{properCase name}}Service,
     private actions$: Actions
   ) { }
 
