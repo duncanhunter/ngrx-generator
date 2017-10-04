e69442ec2d08ba0efbe809b46523aa6db22427cf
Duncan Hunter <duncan@duncanhunter.com.au>
2017-10-04 11:31:09 +1000

    fix - incorrect spacing in actions template

--- templates/_actions.ts
+++ templates/_actions.ts
@@ -5,20 +5,20 @@ export const LOAD_{{upperCase name }}_SUCCESS = '[{{ titleCase name }}] Load Suc
 export const LOAD_{{upperCase name }}_FAIL = '[{{ titleCase name }}] Load Fail';
 
 export class Load {{titleCase name }}Action implements Action {
-  readonly type = LOAD_{{ upperCase name } };
+  readonly type = LOAD_{{ upperCase name }};
 }
 
 export class Load {{titleCase name }}SuccessAction implements Action {
-  readonly type = LOAD_{{ upperCase name } } _SUCCESS;
+  readonly type = LOAD_{{ upperCase name }} _SUCCESS;
   constructor(public payload: any) { }
 }
 
 export class Load {{titleCase name }}FailAction implements Action {
-  readonly type = LOAD_{{ upperCase name } } _FAIL;
+  readonly type = LOAD_{{ upperCase name }} _FAIL;
   constructor(public payload: any) { }
 }
 
 export type Actions =
-  | Load{{ titleCase name } } Action
-  | Load{{ titleCase name } } SuccessAction
-  | Load{{ titleCase name } } FailAction;
+  | Load{{ titleCase name }} Action
+  | Load{{ titleCase name }} SuccessAction
+  | Load{{ titleCase name }} FailAction;
