687a957219d31aead69d8236b1aaa684b4f9e262
Duncan Hunter <duncan@duncanhunter.com.au>
2017-10-06 14:51:20 +1000

    update effect template to return with action creators

--- templates/_effect.ts
+++ templates/_effect.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
+import { switchMap, map } from 'rxjs/operators';
+import { SwaggerClient } from './../../swagger-client';
 import * as {{camelCase name}}Actions from '{{position "actions"}}/{{dashCase name}}.actions';
-import { switchMap, catchError, map } from 'rxjs/operators';
-import { of } from 'rxjs/Observable/of';
-import { {{properCase name}}Service } from './../../swagger-client';
-import { ErrorHandlerService } from '../../services/error-handler.service';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class {{ properCase name }}Effects {
 
   constructor(
-    private {{ camelCase name }}Service: {{properCase name}}Service,
+    private swaggerClient: SwaggerClient,
     private actions$: Actions
   ) { }
   
   @Effect() get$ = this.actions$
      .ofType({{ camelCase name }}Actions.LOAD_{{upperCase name}})
      .pipe(
-        switchMap(payload => this.{{camelCase name}}Service.get()),
-        map(data => ({ type: {{camelCase name}}Actions.LOAD_{{upperCase name }}_SUCCESS, payload: data}),
-        catchError(() => Observable.of({ type: {{camelCase name}}Actions.LOAD_{{upperCase name }}_FAIL}))
-     ));
-
+        switchMap(payload => this.swaggerClient.get()),
+        map(data => (new {{camelCase name}}Actions.load_{{properCase name }}Success(payload: data))
+     )).catch(() => Observable.of(new {{camelCase name}}Actions.load_{{properCase name }}Fail(payload: data)));
 
 }
